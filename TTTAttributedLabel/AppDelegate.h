547d760d7112dd511c44aead8e92c782fbc9958d
姚友秀 <chanpinzhongxin@chanpinzhongxindeMacBook-Pro.local>
2018-03-09 09:24:35 +0800

    Initial Commit

---
+++ TTTAttributedLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTTAttributedLabel
+//
+//  Created by chanpinzhongxin on 2018/3/9.
+//  Copyright © 2018年 chanpinzhongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTTAttributedLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTTAttributedLabel
+//
+//  Created by chanpinzhongxin on 2018/3/9.
+//  Copyright © 2018年 chanpinzhongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
